4b459a4d1fdeaf4a8e77d08cf379ba16fb8f64d5
Malgorzata Stencel <mstencel@f0r4s22.codam.nl>
2025-02-06 15:34:07 +0100

    ray breaks too fast on y

--- src/render/rays.c
+++ src/render/rays.c
@@ -6,7 +6,7 @@
 /*   By: mstencel <mstencel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/03 12:24:23 by mstencel      #+#    #+#                 */
-/*   Updated: 2025/02/06 15:01:10 by mstencel      ########   odam.nl         */
+/*   Updated: 2025/02/06 15:33:07 by mstencel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static void	get_wall(t_root *data, int step_x, int step_y)
 {
 	while (1)
 	{
-		if (data->ray.x_offset < data->ray.y_offset)
+		if (data->ray.x_offset <= data->ray.y_offset)
 		{
 			data->ray.x_offset += data->ray.steps_x;
 			data->ray.x_map += step_x;
@@ -36,9 +36,12 @@ static void	get_wall(t_root *data, int step_x, int step_y)
 		}
 		if (data->ray.x_map < 0 || data->ray.x_map >= data->map.height
 			|| data->ray.y_map < 0 || data->ray.y_map >= data->map.lenght)
-			break ; //it's outside of the map values
-		if (data->map.map[(int)data->ray.y_map][(int)data->ray.x_map] == '1')
-			break ; //found the wall
+		{
+			printf("I hit the wall on the %d %d\n", data->ray.y_map, data->ray.x_map);
+			break ; //it's outside of the map values or found the wall
+		}
+		if (data->map.map[data->ray.y_map][data->ray.x_map] == '1')
+			break;
 	}
 }
 
